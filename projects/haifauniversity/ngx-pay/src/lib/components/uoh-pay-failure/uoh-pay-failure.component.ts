4e201a1b3a7e88ce1a67f01c434ea7d1af284046
Pablo Saracusti <psaracu1@univ.haifa.ac.il>
2020-11-25 09:16:53 +0200

    refactor(components): Input in success & failure

--- projects/haifauniversity/ngx-pay/src/lib/components/uoh-pay-failure/uoh-pay-failure.component.ts
+++ projects/haifauniversity/ngx-pay/src/lib/components/uoh-pay-failure/uoh-pay-failure.component.ts
@@ -1,23 +1,17 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, HostBinding, Input } from '@angular/core';
 import { UohPayment } from '../../models/payment.model';
 import { UohPayStatus } from '../../models/status.model';
-import { UohPay } from '../../services/uoh-pay.service';
 
 @Component({
   selector: 'uoh-pay-failure',
   templateUrl: './uoh-pay-failure.component.html',
   styleUrls: ['./uoh-pay-failure.component.css'],
 })
-export class UohPayFailureComponent implements OnInit {
-  payment$: Observable<UohPayment>;
+export class UohPayFailureComponent {
   status = UohPayStatus;
+  @Input() payment: UohPayment;
   @Input() token: string;
   @HostBinding('class') class = 'uoh-pay-failure';
 
-  constructor(private pay: UohPay) {}
-
-  ngOnInit(): void {
-    this.payment$ = this.pay.payment$;
-  }
+  constructor() {}
 }
--- projects/haifauniversity/ngx-pay/src/lib/components/uoh-pay-success/uoh-pay-success.component.ts
+++ projects/haifauniversity/ngx-pay/src/lib/components/uoh-pay-success/uoh-pay-success.component.ts
@@ -1,20 +1,14 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, HostBinding, Input } from '@angular/core';
 import { UohPayment } from '../../models/payment.model';
-import { UohPay } from '../../services/uoh-pay.service';
 
 @Component({
   selector: 'uoh-pay-success',
   templateUrl: './uoh-pay-success.component.html',
   styleUrls: ['./uoh-pay-success.component.css'],
 })
-export class UohPaySuccessComponent implements OnInit {
-  payment$: Observable<UohPayment>;
+export class UohPaySuccessComponent {
+  @Input() payment: UohPayment;
   @HostBinding('class') class = 'uoh-pay-success';
 
-  constructor(private pay: UohPay) {}
-
-  ngOnInit(): void {
-    this.payment$ = this.pay.payment$;
-  }
+  constructor() {}
 }
