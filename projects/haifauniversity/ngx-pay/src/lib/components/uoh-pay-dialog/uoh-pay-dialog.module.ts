ddf92aeedb12c9d28985522af4dd8b03fd14c358
Pablo Saracusti <psaracu1@univ.haifa.ac.il>
2020-11-26 14:54:28 +0200

    fix(uoh-pay-dialog): Import mat button module

--- projects/haifauniversity/ngx-pay/src/lib/components/uoh-pay-dialog/uoh-pay-dialog.module.ts
+++ projects/haifauniversity/ngx-pay/src/lib/components/uoh-pay-dialog/uoh-pay-dialog.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 import { UohPayDialogComponent } from './uoh-pay-dialog.component';
 
 @NgModule({
-  imports: [CommonModule, MatDialogModule, MatIconModule],
+  imports: [CommonModule, MatDialogModule, MatIconModule, MatButtonModule],
   declarations: [UohPayDialogComponent],
   exports: [UohPayDialogComponent],
   entryComponents: [UohPayDialogComponent],
