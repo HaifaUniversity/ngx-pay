54f52cd96c2b4c786f95315bc8cce3a35cebfa9a
Pablo Saracusti <psaracu1@univ.haifa.ac.il>
2020-12-03 16:26:37 +0200

    refactor(uoh-pay-connectivity): Add logs

--- projects/haifauniversity/ngx-pay/src/lib/services/uoh-pay-connectivity.service.ts
+++ projects/haifauniversity/ngx-pay/src/lib/services/uoh-pay-connectivity.service.ts
@@ -2,13 +2,14 @@ import { Injectable, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { UohLogger } from '@haifauniversity/ngx-tools';
 import { BehaviorSubject, Observable, of, Subscription, timer } from 'rxjs';
-import { catchError, distinctUntilChanged, takeUntil, map, filter } from 'rxjs/operators';
+import { catchError, distinctUntilChanged, takeUntil, map, filter, tap } from 'rxjs/operators';
 import { UohPaySlowConnectionDialogComponent } from '../components/uoh-pay-dialog/uoh-pay-slow-connection-dialog/uoh-pay-slow-connection-dialog.component';
 import { UohPayUnreachableDialogComponent } from '../components/uoh-pay-dialog/uoh-pay-unreachable-dialog/uoh-pay-unreachable-dialog.component';
 import { UohPay } from './uoh-pay.service';
 
 @Injectable()
 export class UohPayConnectivity implements OnDestroy {
+  private readonly TIMEOUT = 10000;
   private success = new BehaviorSubject<boolean>(undefined);
   private subscription = new Subscription();
 
@@ -19,13 +20,17 @@ export class UohPayConnectivity implements OnDestroy {
   }
 
   check(token: string): Observable<boolean> {
+    this.logger.debug(`[UohPayConnectivity.check] Check connection for token ${token}`);
     const ping$ = this.pay.get(token).pipe(
       map((_) => true),
       catchError((_) => of(false))
     );
     this.subscription.add(
-      timer(10000)
-        .pipe(takeUntil(ping$))
+      timer(this.TIMEOUT)
+        .pipe(
+          tap((_) => this.logger.debug(`[UohPayConnectivity.check] Slow connection timeout: ${this.TIMEOUT}`)),
+          takeUntil(ping$)
+        )
         .subscribe((_) => this.dialog.open(UohPaySlowConnectionDialogComponent, { disableClose: false }))
     );
     this.subscription.add(ping$.subscribe((status) => this.handleStatus(token, status)));
@@ -37,6 +42,7 @@ export class UohPayConnectivity implements OnDestroy {
   }
 
   private handleStatus(token: string, success: boolean): void {
+    this.logger.debug(`[UohPayConnectivity.handleStatus] For token ${token} success: ${success}`);
     this.dialog.closeAll();
     if (success) {
       this.success.next(true);
@@ -50,6 +56,7 @@ export class UohPayConnectivity implements OnDestroy {
             if (retry) {
               this.check(token);
             } else {
+              this.logger.debug(`[UohPayConnectivity.handleStatus] For token ${token} unreachable`);
               this.success.next(false);
             }
           })
