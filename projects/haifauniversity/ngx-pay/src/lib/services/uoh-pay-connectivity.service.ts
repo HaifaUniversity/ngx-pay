2651b3e6bc936c5a6b78fa70598c559f433c9e8c
Pablo Saracusti <psaracu1@univ.haifa.ac.il>
2020-12-03 16:30:30 +0200

    refactor(uoh-pay-connectivity): Add comments

--- projects/haifauniversity/ngx-pay/src/lib/services/uoh-pay-connectivity.service.ts
+++ projects/haifauniversity/ngx-pay/src/lib/services/uoh-pay-connectivity.service.ts
@@ -7,6 +7,9 @@ import { UohPaySlowConnectionDialogComponent } from '../components/uoh-pay-dialo
 import { UohPayUnreachableDialogComponent } from '../components/uoh-pay-dialog/uoh-pay-unreachable-dialog/uoh-pay-unreachable-dialog.component';
 import { UohPay } from './uoh-pay.service';
 
+/**
+ * Checks the connectivity with the webservice before proceeding with the payment.
+ */
 @Injectable()
 export class UohPayConnectivity implements OnDestroy {
   private readonly TIMEOUT = 10000;
@@ -19,6 +22,10 @@ export class UohPayConnectivity implements OnDestroy {
     this.subscription.unsubscribe();
   }
 
+  /**
+   * Checks the connectivity and raises messages when the connection is slow or when the webservice cannot be reached.
+   * @param token The token to perform the check.
+   */
   check(token: string): Observable<boolean> {
     this.logger.debug(`[UohPayConnectivity.check] Check connection for token ${token}`);
     const ping$ = this.pay.get(token).pipe(
@@ -41,6 +48,11 @@ export class UohPayConnectivity implements OnDestroy {
     );
   }
 
+  /**
+   * Handles the status of the check. If it fails it shows a message with the option to retry.
+   * @param token The token to perform the check.
+   * @param success The result from the check.
+   */
   private handleStatus(token: string, success: boolean): void {
     this.logger.debug(`[UohPayConnectivity.handleStatus] For token ${token} success: ${success}`);
     this.dialog.closeAll();
