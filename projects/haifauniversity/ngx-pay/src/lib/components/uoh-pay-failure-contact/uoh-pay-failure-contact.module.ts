3c69f6016c1b01485072174edac1fd2c5331d414
Pablo Saracusti <psaracu1@univ.haifa.ac.il>
2020-12-09 11:52:53 +0200

    refactor(uoh-pay-failure-contact): As button

--- projects/haifauniversity/ngx-pay/src/lib/components/uoh-pay-failure-contact/uoh-pay-failure-contact.module.ts
+++ projects/haifauniversity/ngx-pay/src/lib/components/uoh-pay-failure-contact/uoh-pay-failure-contact.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { UohPayFailureContactComponent } from './uoh-pay-failure-contact.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   declarations: [UohPayFailureContactComponent],
   exports: [UohPayFailureContactComponent],
 })
