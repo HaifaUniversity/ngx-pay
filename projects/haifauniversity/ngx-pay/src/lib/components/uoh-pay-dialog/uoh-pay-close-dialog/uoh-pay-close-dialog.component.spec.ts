03001142bece27fba691b04f9ccdcab8daede12b
Pablo Saracusti <psaracu1@univ.haifa.ac.il>
2020-12-09 09:21:05 +0200

    chore: Upgrade to angular 11

--- projects/haifauniversity/ngx-pay/src/lib/components/uoh-pay-dialog/uoh-pay-close-dialog/uoh-pay-close-dialog.component.spec.ts
+++ projects/haifauniversity/ngx-pay/src/lib/components/uoh-pay-dialog/uoh-pay-close-dialog/uoh-pay-close-dialog.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { UohPayCloseDialogComponent } from './uoh-pay-close-dialog.component';
 
@@ -6,7 +6,7 @@ describe('UohPayCloseDialogComponent', () => {
   let component: UohPayCloseDialogComponent;
   let fixture: ComponentFixture<UohPayCloseDialogComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [UohPayCloseDialogComponent],
     }).compileComponents();
