2347bd631e64761ca980bc94562e4c033ab216f1
tovaUniv <tfrayman@univ.haifa.ac.il>
2022-09-22 13:51:43 +0300

    add description of code error

--- projects/haifauniversity/ngx-pay/src/lib/components/uoh-pay-failure/uoh-pay-failure.component.ts
+++ projects/haifauniversity/ngx-pay/src/lib/components/uoh-pay-failure/uoh-pay-failure.component.ts
@@ -2,7 +2,7 @@ import { Component, HostBinding, Inject, Input, OnInit } from '@angular/core';
 import { UohLogger } from '@haifauniversity/ngx-tools';
 import { UohPayContact, UOH_PAY_CONTACT } from '../../models/contact.model';
 import { UohPayment } from '../../models/payment.model';
-import { UohPayStatus } from '../../models/status.model';
+import { errorCods, UohPayStatus } from '../../models/status.model';
 
 @Component({
   selector: 'uoh-pay-failure',
@@ -11,6 +11,7 @@ import { UohPayStatus } from '../../models/status.model';
 })
 export class UohPayFailureComponent implements OnInit {
   status = UohPayStatus;
+  errors = errorCods;
   /**
    * The payment details.
    */
@@ -20,10 +21,13 @@ export class UohPayFailureComponent implements OnInit {
    */
   @Input() token: string;
   @HostBinding('class') class = 'uoh-pay-failure';
+  error: any;
 
-  constructor(private logger: UohLogger, @Inject(UOH_PAY_CONTACT) public contact: UohPayContact) {}
+  constructor(private logger: UohLogger, @Inject(UOH_PAY_CONTACT) public contact: UohPayContact) { }
 
   ngOnInit(): void {
+    this.error = this.errors.find(x => x.key == this.payment.creditResponse)?.value;
+    this.error = this.error != null && this.error != undefined ? this.error : this.payment.creditResponse;
     this.logger.error(
       `[UohPayFailureComponent.ngOnInit] Payment failed for token ${this.token} with status ${this.payment.status}.`
     );
